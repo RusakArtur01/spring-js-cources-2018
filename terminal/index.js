1bfd3321d445ee19b4e99c3f2e8d943dc8a8ed65
RusakArtur01 <artur6200065@gmail.com>
2018-03-23 00:40:00 +0300

    for testing we will use simple id.

--- terminal/index.js
+++ terminal/index.js
@@ -60,12 +60,17 @@ function writeFile(data) {
 }
 
 function guid() {
-  function s4() {
-    return Math.floor((1 + Math.random()) * 0x10000)
-      .toString(16)
-      .substring(1);
-  }
-  return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+    function s4(){
+        // return Math.floor((1 + Math.random()) * 0x10000)
+        //   .toString(16)
+        //   .substring(1);
+        //--------------------------------------------
+        // for testing we will use simple id.
+        return Math.floor(Math.random() * (20 - 1) + 1);
+
+    }
+    return s4();
+    //return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
 }
 
 // Craft questions to present to users
